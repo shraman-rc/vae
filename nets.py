6b4d6ae40962eeafea586f97bda5a7ab7538f2c3
shraman <shraman@mit.edu>
2016-05-02 16:31:12 -0400

    incorporated modularized NN construction with rest of code

--- nets.py
+++ nets.py
@@ -1,3 +1,9 @@
+'''
+nets.py: Defines and constructs basic neural net architectures for the auto-encoder
+
+Construction closely follows [1]: Appendix C
+'''
+
 import tensorflow as tf
 import tf_helpers as tfh
 from collections import namedtuple
@@ -7,7 +13,8 @@ DEFAULT_LAYER_SIZES = [100]
 DEFAULT_LATENT_DIM = 10
 
 BernoulliParam = namedtuple('BernoulliParam', ['p'])
-GaussianParam = namedtuple('GaussianParam', ['mu', 'sigma'])
+GaussianParam = namedtuple('GaussianParam', ['mu', 'log_var'])
+
 
 class MLP(object):
 
@@ -20,22 +27,25 @@ class MLP(object):
         '''
         Params:
             - input_batch: The input tensor to the MLP
+                - NOTE: This should be a 'tf.placeholder'
             - layer_sizes: The number of weights in each hidden layer
             - dim: dimension of the resulting probability distribution
         '''
-        self.batch_size, self.input_size = tfh.shape(input_batch)
+        self.batch_size, self.input_dim = tfh.shape(input_batch)
+        self.input_batch = input_batch
         self.layer_sizes = layer_sizes
         self.out_dim = dim
         # Generate hidden layer vars
-        self.weights = [tf.Variable(tf.zeros([self.input_size, layer_sizes[0]]))]
+        self.weights = [tf.Variable(tf.zeros([self.input_dim, layer_sizes[0]]))]
         self.biases = [tf.Variable(tf.zeros([layer_sizes[0]]))]
-        for in_size,out_size in zip(layer_sizes, layer_sizes[1:]):
+        for in_dim,out_dim in zip(layer_sizes, layer_sizes[1:]):
             # TODO: Use tf.contrib.layers.xavier_initializer(...)
-            self.weights.append(tf.Variable(tf.zeros([in_size, out_size])))
-            self.biases.append(tf.Variable(tf.zeros([out_size])))
+            self.weights.append(tf.Variable(tf.zeros([in_dim, out_dim])))
+            self.biases.append(tf.Variable(tf.zeros([out_dim])))
 
         # Generate output vars (parameters of a distribution)
         self.params = self._gen_params()
+
 
 class BernoulliMLP(MLP):
 
@@ -45,14 +55,58 @@ class BernoulliMLP(MLP):
 
     def _gen_params(self):
         '''
-        Setup TF computation graph to compute the output value that
+        Setup TF computation graph (neural net) to compute the value that
             parameterizes the Bernoulli distribution
         '''
-        # TODO: Extend this to be multilayer
         param = BernoulliParam()
-        bias_p = tf.Variable(tf.zeros([self.out_dim]))
-        weights_p = tf.Variable(tf.zeros([self.layer_sizes[-1], self.out_dim]))
 
-        param.p = tf.sigmoid(tf.matmul( , ) + bias_p)
+        # TODO: Extend this to be multilayer
+        hidden_weights, hidden_bias, hidden_size = \
+            self.weights[0], self.biases[0], self.layer_sizes[0]
+        self.bias_out = tf.Variable(tf.zeros([self.out_dim]))
+        self.weights_out = tf.Variable(tf.zeros([hidden_size, self.out_dim]))
+
+        # TODO: Tru ReLU instead of tanh
+        param.p = tf.sigmoid(
+            tf.matmul(
+                tf.tanh(tf.matmul(self.input_batch, hidden_weights) + hidden_bias),
+                self.weights_out)
+            + self.bias_out)
+
+        return param
+
+
+class GaussianMLP(MLP):
+
+    def __init__(self, input_batch,
+            layer_sizes=DEFAULT_LAYER_SIZES, dim=DEFAULT_LATENT_DIM):
+        super(self.__class__, self).__init__(input_batch, layer_sizes, dim)
+
+    def _gen_params(self):
+        '''
+        Setup TF computation graph (neural net) to compute the values that
+            parameterize the Gaussian distribution
+
+        NOTE: The variance parameter output is actually log(variance) as
+            suggested by [1]
+        '''
+        param = GaussianParam()
+
+        # TODO: Extend this to be multilayer
+        hidden_weights, hidden_bias, hidden_size = \
+            self.weights[0], self.biases[0], self.layer_sizes[0]
+        self.bias_mu = tf.Variable(tf.zeros([self.out_dim]))
+        self.weights_mu = tf.Variable(tf.zeros([hidden_size, self.out_dim]))
+        self.bias_logvar = tf.Variable(tf.zeros([self.out_dim]))
+        self.weights_logvar = tf.Variable(tf.zeros([hidden_size, self.out_dim]))
+
+        # TODO: Tru ReLU instead of tanh
+        h = tf.tanh(
+            tf.matmul(self.input_batch, hidden_weights) + hidden_bias)
+        param.mu = tf.matmul(h, self.weights_mu) + self.bias_mu
+        param.log_var = tf.matmul(h, self.weights_logvar) + self.bias_logvar
+            # TODO: Why do we use log variance? Do we expect
+            # extremely high/low values? Does it help with
+            # vanishing weights phenomenon?
 
         return param
--- vae.py
+++ vae.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 import numpy as np
 
-from likelihoods import ll_bernoulli, ll_gaussian
+from nets import BernoulliMLP, GaussianMLP
+import likelihoods as lh
 
 MNIST_FLAT_DIM = 784    # Flattened dimension of MNIST images, 'x'
 HIDDEN_LAYERS_ENC = 500
@@ -16,64 +17,67 @@ T = 10000       # Training iterations
 ADG_RATE = 0.01 # Adagrad global learning rate (chosen from
                 #   {0.01,0.02,0.1} -- cf. [1]: Section 5)
 
-# Encoder construction (Gaussian) -- cf. [1]: Appendix C
-
-# Inputs - placeholder for mini-batches of (flattened) images
+# Inputs - mini-batches of (flattened) images
 x_batch = tf.placeholder(tf.float32, shape=[M, MNIST_FLAT_DIM])
 
-# Weights
-W_hidden_e = tf.Variable(tf.zeros([MNIST_FLAT_DIM, HIDDEN_LAYERS_ENC]))
-W_mu_q = tf.Variable(tf.zeros([HIDDEN_LAYERS_ENC, J]))
-W_log_sigma_q = tf.Variable(tf.zeros([HIDDEN_LAYERS_ENC, J]))
+encoder = GaussianMLP(x_batch) # Gaussian q(z|x)
 
-# Biases
-b_hidden_e = tf.Variable(tf.zeros([HIDDEN_LAYERS_ENC]))
-b_mu_q = tf.Variable(tf.zeros([J]))
-b_log_sigma_q = tf.Variable(tf.zeros([J]))
-
-# Activations/Outputs
-h_e = tf.tanh(  # TODO: try ReLU instead of tanh?
-        tf.matmul(x_batch, W_hidden_e) + b_hidden_e)
-mu_q = tf.matmul(h_e, W_mu_q) + b_mu_q
-log_sigma_sq_q = tf.matmul(h_e, W_log_sigma_q) + b_log_sigma_q)
-    # TODO: Why do we use log variance? Do we expect
-    # extremely high/low values? Does it help with
-    # vanishing weights phenomenon?
-sigma_sq_q = tf.exp(log_sigma_sq_q)
-sigma_q = tf.sqrt(sigma_sq_q)
-
-# Decoder construction (Gaussian) -- cf. [1]: Appendix C
-
-# Inputs - latent space samples w/ reparameterization (z = g(ep,x); ep ~ p(ep))
-#   This outsources randomness to auxiliary variable (see [1]: Sec 2.4)
+# Bridge between encoder and decoder
+# Latent space samples w/ reparameterization (z = g(ep,x); ep ~ p(ep))
+#   This outsources randomness to auxiliary r.v. (see [1]: Sec 2.4)
 #   For a Gaussian 'q' we have: g(ep,x) = mu + sigma*ep, and p(ep) = N(0,I)
 # Note: Element-wise univariate Gaussian sampling <=> multivariate Gaussian sampling
 # TODO: Introduce L later here
-ep = tf.random_normal([M, J], mean=0, stddev=1) # TODO: Do we resample this everytime?
-                                                # TODO: Is TF resampling everytime?
-z = mu_q + sigma_q*ep # element-wise ops, z is MxJ, one latent per datapoint
+ep = tf.random_normal([M, J], mean=0, stddev=1) # TODO: Is TF resampling everytime?
+z_batch = mu_q + sigma_q*ep # element-wise ops, z_batch has latent per datapoint (MxJ)
 
-# Weights
-W_hidden_d = tf.Variable(tf.zeros([J, HIDDEN_LAYERS_DEC]))
-W_mu_p = tf.Variable(tf.zeros([HIDDEN_LAYERS_DEC, MNIST_FLAT_DIM]))
-W_log_sigma_p = tf.Variable(tf.zeros([HIDDEN_LAYERS_DEC, MNIST_FLAT_DIM]))
+decoder = BernoulliMLP(z_batch)
 
-# Biases
-b_hidden_d = tf.Variable(tf.zeros([HIDDEN_LAYERS_DEC]))
-b_mu_p = tf.Variable(tf.zeros([MNIST_FLAT_DIM]))
-b_log_sigma_p = tf.Variable(tf.zeros([MNIST_FLAT_DIM]))
+#
+## Weights
+#W_hidden_e = tf.Variable(tf.zeros([MNIST_FLAT_DIM, HIDDEN_LAYERS_ENC]))
+#W_mu_q = tf.Variable(tf.zeros([HIDDEN_LAYERS_ENC, J]))
+#W_log_sigma_q = tf.Variable(tf.zeros([HIDDEN_LAYERS_ENC, J]))
+#
+## Biases
+#b_hidden_e = tf.Variable(tf.zeros([HIDDEN_LAYERS_ENC]))
+#b_mu_q = tf.Variable(tf.zeros([J]))
+#b_log_sigma_q = tf.Variable(tf.zeros([J]))
+#
+## Activations/Outputs
+#h_e = tf.tanh(  # TODO: try ReLU instead of tanh?
+#        tf.matmul(x_batch, W_hidden_e) + b_hidden_e)
+#mu_q = tf.matmul(h_e, W_mu_q) + b_mu_q
+#log_sigma_sq_q = tf.matmul(h_e, W_log_sigma_q) + b_log_sigma_q)
+#    # TODO: Why do we use log variance? Do we expect
+#    # extremely high/low values? Does it help with
+#    # vanishing weights phenomenon?
+#sigma_sq_q = tf.exp(log_sigma_sq_q)
+#sigma_q = tf.sqrt(sigma_sq_q)
 
-# Activations/Outputs
-h_d = tf.tanh(  # TODO: try ReLU instead of tanh?
-        tf.matmul(z, W_hidden_d) + b_hidden_d)
-mu_p = tf.matmul(h_d, W_mu_p) + b_mu_p
-log_sigma_sq_p = tf.matmul(h_d, W_log_sigma_p) + b_log_sigma_p)
-    # TODO: Why do we use log variance? Do we expect
-    # extremely high/low values? Does it help with
-    # vanishing weights phenomenon?
-sigma_p = tf.sqrt(tf.exp(log_sigma_sq_p))
+# Decoder construction (Bernoulli, for MNIST)
+
+## Weights
+#W_hidden_d = tf.Variable(tf.zeros([J, HIDDEN_LAYERS_DEC]))
+#W_mu_p = tf.Variable(tf.zeros([HIDDEN_LAYERS_DEC, MNIST_FLAT_DIM]))
+#W_log_sigma_p = tf.Variable(tf.zeros([HIDDEN_LAYERS_DEC, MNIST_FLAT_DIM]))
+#
+## Biases
+#b_hidden_d = tf.Variable(tf.zeros([HIDDEN_LAYERS_DEC]))
+#b_mu_p = tf.Variable(tf.zeros([MNIST_FLAT_DIM]))
+#b_log_sigma_p = tf.Variable(tf.zeros([MNIST_FLAT_DIM]))
+#
+## Activations/Outputs
+#h_d = tf.tanh(  # TODO: try ReLU instead of tanh?
+#        tf.matmul(z, W_hidden_d) + b_hidden_d)
+#mu_p = tf.matmul(h_d, W_mu_p) + b_mu_p
+#log_sigma_sq_p = tf.matmul(h_d, W_log_sigma_p) + b_log_sigma_p)
+#    # TODO: Why do we use log variance? Do we expect
+#    # extremely high/low values? Does it help with
+#    # vanishing weights phenomenon?
 
 # ELBO estimator construction
+sigma_p = tf.sqrt(tf.exp(log_sigma_sq_p))
 
 # The KL divergence between the variational approx. and the prior p_theta(z) acts as
 #   a regularizing term so that the latent variables don't overfit. The closed-form
@@ -81,7 +85,7 @@ sigma_p = tf.sqrt(tf.exp(log_sigma_sq_p))
 # TODO: Why would overfitting be a problem in the auto-encoding scenario? Wouldn't
 #   overfitting lead to a better likelihood lower bound measure used in [1]'s experiments?
 KL_prior_regularizer = 0.5 * tf.reduce_sum(1 + log_sigma_sq_q - mu_q - sigma_sq_q, 1)
-pred_reconstr_err = ll_bernoulli()  # Measures log p_theta(x_i|z) # TODO: Try a different loss function? Cross entropy?
+pred_reconstr_err = lh.ll_bernoulli()  # Measures log p_theta(x_i|z) # TODO: Try a different loss function? Cross entropy?
 
 # ^ TODO: CHANGE TO BERNOULLI AND COMPARTMENTALIZE CODE (backends.py) WE TAKE ELEMENT WISE SIGMOID TO PARAMETERIZE
 # MULTIVARIATE BERNOULLI (i.e. pixel values in image - note will need to rescale if doing on regular images) BUT
