fixed KL divergence, added visualizations, testing

likelihoods.py
@@ -1,6 +1,7 @@
-'''
-likelihoods.py: TF implementations of closed-form likelihoods given data/parameters
-'''
+""" likelihoods.py: TF implementations of closed-form likelihoods given data/parameters
+"""
+
+__author__ = "shraman-rc"
 
 import tensorflow as tf
 from collections import namedtuple
@@ -29,7 +30,7 @@ def ll_bernoulli(data, rho):
     # Taking the log of this (only the probabilities rho_i, not the
     # 0-1 coefficients x_i) we get our log-likelihood:
     #   \sum_i {x_i*log(rho_i) + (1-x_i)*log(1-rho_i)}
-    return tf.reduce_sum(data*tf.log(rho) + (1-data)*tf.log(1-rho), 1)
+    return tf.reduce_sum(data*tf.log(1e-10 + rho) + (1-data)*tf.log(1e-10 + 1-rho), 1)
 
 def ll_gaussian(data, mu, log_var):
     '''

nets.py
@@ -1,8 +1,9 @@
-'''
-nets.py: Defines and constructs basic neural net architectures for the auto-encoder
+""" nets.py: Defines and recontructs basic neural net architectures for the auto-encoder
 
 Construction closely follows [1]: Appendix C
-'''
+"""
+
+__author__ = "shraman-rc"
 
 import tensorflow as tf
 import tf_helpers as tfh
@@ -67,7 +68,7 @@ class BernoulliMLP(MLP):
         # TODO: Tru ReLU instead of tanh
         p = tf.sigmoid(
             tf.matmul(
-                tf.tanh(tf.matmul(self.input_batch, hidden_weights) + hidden_bias),
+                tf.nn.relu(tf.matmul(self.input_batch, hidden_weights) + hidden_bias),
                 self.weights_out)
             + self.bias_out)
 
@@ -97,7 +98,7 @@ class GaussianMLP(MLP):
         self.weights_logvar = tf.Variable(tf.zeros([hidden_size, self.out_dim]))
 
         # TODO: Tru ReLU instead of tanh
-        h = tf.tanh(
+        h = tf.nn.relu(
             tf.matmul(self.input_batch, hidden_weights) + hidden_bias)
         mu = tf.matmul(h, self.weights_mu) + self.bias_mu
         log_var = tf.matmul(h, self.weights_logvar) + self.bias_logvar

vae.py
@@ -1,9 +1,17 @@
+#!/usr/bin/python
+
+""" vae.py: Run the Variational Auto-encoder.
+"""
+
+__author__ = "shraman-rc"
+
 import tensorflow as tf
 import numpy as np
 import click as cl
 
 from nets import BernoulliMLP, GaussianMLP
 import likelihoods as lh
+import vis
 
 MNIST_FLAT_DIM = 784    # Flattened dimension of MNIST images, 'x'
 HIDDEN_LAYER_ENC = 500
@@ -14,8 +22,8 @@ HIDDEN_LAYER_DEC = 200
 M = 100         # minibatch size
 L = 1           # MCE samples
 J = 10          # Dimensionality of latent space, 'z'
-T = 2000        # Training iterations
-ADG_RATE = 0.01 # Adagrad global learning rate (chosen from
+T = 5000        # Training iterations
+ADG_RATE = 0.1 # Adagrad global learning rate (chosen from
                 #   {0.01,0.02,0.1} -- cf. [1]: Section 5)
 
 # Inputs - mini-batches of (flattened) images
@@ -81,12 +89,13 @@ decoder = BernoulliMLP(z_batch, [HIDDEN_LAYER_DEC], MNIST_FLAT_DIM)
 #    # extremely high/low values? Does it help with
 #    # vanishing weights phenomenon?
 
-# The KL divergence between the variational approx. and the *prior* p_theta(z) acts as
-#   a regularizing term so that the latent distribution doesn't overfit. The closed-form
-#   eq. is derived in [1]: Appedix B
+# The (negative) KL divergence between the variational approx. and the *prior*
+#   p_theta(z) acts as a regularizing term so that the latent distribution
+#   doesn't overfit. The closed-form eq. is derived in [1]: Appedix B
 # TODO: Why would overfitting be a problem in the auto-encoding scenario? Wouldn't
 #   overfitting lead to a better likelihood lower bound measure used in [1]'s experiments?
-KL_regularizer = 0.5 * tf.reduce_sum(1 + log_var_q - mu_q - var_q, 1)
+# TODO: Bug changing mu_q to mu_q**2 makes log likelihood go to zero...
+KL_regularizer = 0.5 * tf.reduce_sum(1 + log_var_q - mu_q**2 - var_q, 1)
 
 # The 'reconstruction error' (predictive likelihood): log p_theta(x_batch|z)
 # TODO: Try a different loss function? Cross entropy?
@@ -108,12 +117,33 @@ train_op = tf.train.AdagradOptimizer(ADG_RATE).minimize(-ELBO_estimate)
 # Train on MNIST
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+ELBOs = np.zeros(T)
+KLs = np.zeros(T)
+LLs = np.zeros(T)
+
 with tf.Session() as sess:
     sess.run(tf.initialize_all_variables())
-    for t in range(T):
+    ELBO, neg_KL, ll = 0,0,np.NINF
+    # Train TODO: Move into MLP class
+    iters = xrange(T)
+    for t in iters:
         cl.secho('Minibatch {}'.format(t), fg='green', bold=False)
         batch = mnist.train.next_batch(M)
-        train_op.run(feed_dict={x_batch: batch[0]})
+        _, ELBO, ll, neg_KL = sess.run(
+            [train_op, ELBO_estimate, reconstr_err, KL_regularizer],
+            feed_dict={x_batch: batch[0]})
+        ELBO, neg_KL, ll = np.mean(ELBO), np.mean(neg_KL), np.mean(ll)
+        ELBOs[t] = ELBO; KLs[t] = -neg_KL; LLs[t] = ll; opts[t] = opt
+        cl.secho('ELBO (estimate): {}\nKL Div (prior): {}\nLikelihood: {}'
+            .format(ELBO, -neg_KL, ll), fg='cyan')
+
+    # Tests
+    titles = ["$\mathcal{L}(\phi,\\theta;x)$",
+              "$KL(q_{\phi}(z|x)||p_{\\theta}(z))$",
+              "$\log(p_{\\theta}(x|z))$"]
+    params = {"\eta_{Adagrad}":ADG_RATE, "Activation":"ReLU"}
+    vis.basic_multiplot([iters]*3,[[ELBOs], [KLs], [LLs]], titles, show_legend=False, params=params)
+    
 
 # TODO: When implementating Variational Dropout, can use tf.nn.dropout
 # TODO: Record error vectors